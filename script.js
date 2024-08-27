c61d3fd8002aa3fd9382357709728318694ecf73
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-08-27 14:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById('openModal').onclick = function() {
+    document.getElementById('myModal').style.display = "block";
+};
+
+document.querySelector('.close-modal').onclick = function() {
+    document.getElementById('myModal').style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = "none";
+    }
+};
